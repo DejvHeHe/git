5b6d3aceafe2799f73bc681636f836de5458bad1
David Sobíšek <dsobisek04@gmail.com>
2024-11-20 23:38:53 +0100

    Uprava DU5.js

--- DUJS/DU5.js
+++ DUJS/DU5.js
@@ -1,13 +1,35 @@
-let Jmena=["Adam","Tomáš"];
-let pracovnici=[
-{
-    name:"Adam"
-},
-{
-    name:"Adam"
-}
+let Jmena = ["Adam", "Tomáš","David"];
+let pracovnici = [
+    { name: "Adam" },
+    { name: "Adam" },
+    {name:"David"}
 ];
-for (i=0;i<Jmena.length;i++)
-{
-    
+
+let pocet = 0;
+let cetnostPole = [];
+
+for (let i = 0; i < Jmena.length; i++) {
+    let cetnost = {};
+    let AktualniJmeno = Jmena[i];
+    cetnost.name = AktualniJmeno;
+    for (let x = 0; x < pracovnici.length; x++) {
+        if (AktualniJmeno === pracovnici[x].name) {
+            pocet++;
+        }
+    }
+    cetnost.count = pocet;
+    cetnostPole.push(cetnost);
+    pocet = 0; // reset počítadla
 }
+
+// Najít maximum
+let max = 0;
+let maxJmeno = "";
+for (let y = 0; y < cetnostPole.length; y++) {
+    if (cetnostPole[y].count > max) {
+        max = cetnostPole[y].count;
+        maxJmeno = cetnostPole[y].name;
+    }
+}
+console.log(cetnostPole);
+console.log(`Maximální počet: ${max}, Jméno: ${maxJmeno}`);
