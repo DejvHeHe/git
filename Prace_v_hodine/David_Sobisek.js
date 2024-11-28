435e0fbb072407689f46664f407749b6d1f10923
David Sobíšek <dsobisek04@gmail.com>
2024-11-28 14:56:16 +0100

    Přidání složky

---
+++ Prace_v_hodine/David_Sobisek.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const data = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "students_practice_dataset.json"), {
+    encoding: "utf8",
+    flag: "r",
+  })
+);
+let count=0;
+postsList=data.posts;
+
+for(i=0;i<postsList.length;i++)
+{
+   let post=postsList[i];
+   count=post.comments.length
+   console.log(post.post_id+":"+count)
+   if(count>3)
+   {
+    console.log(post)
+   }
+   
+
+}
