19e829c530a76978cdcfee4dcfeb95c6b271892c
David Sobíšek <dsobisek04@gmail.com>
2024-11-30 22:24:42 +0100

    Uprava DU 4 a 5

--- DUJS/DU5.js
+++ DUJS/DU5.js
@@ -1,17 +1,26 @@
 
-let pracovnici = [
-    { name: "Adam" },
-    { name: "Adam" },
-    {name:"David"}
-];
+function main(dtoIn)
+{   const generateEmployeeData = require('./DU3.js');  
+    const pracovnici=generateEmployeeData(dtoIn);
 
-let cetnost={};
-pracovnici.forEach(element => {
-    if (cetnost[element.name] === undefined) {
-        cetnost[element.name] = 1;
-    } else {
-        cetnost[element.name]++;
-    }   
+    let cetnost={};
+    pracovnici.forEach(element => {
+        if (cetnost[element.name] === undefined) {
+            cetnost[element.name] = 1;
+        } else {
+            cetnost[element.name]++;
+        }   
 
-});
-console.log(cetnost);
+    });
+    return cetnost;
+}
+
+const dtoIn = {
+    age: {
+        min: 20,
+        max: 40
+    },
+    count: 50// Počet zaměstnanců, které chceme vygenerovat
+};
+dtoOut=main(dtoIn)
+console.log(JSON.stringify(dtoOut, null, 2))
