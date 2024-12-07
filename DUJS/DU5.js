94caa87b1ecaf5f2c89081b831ccb57be3a58077
David Sobíšek <dsobisek04@gmail.com>
2024-12-07 17:17:29 +0100

    Uprava  du5

--- DUJS/DU5.js
+++ DUJS/DU5.js
@@ -1,25 +1,29 @@
+function main(dtoIn) {
+    const generateEmployeeData = require('./DU3.js');
+    const pracovnici = generateEmployeeData(dtoIn);
 
-function main(dtoIn)
-{   const generateEmployeeData = require('./DU3.js');  
-    const pracovnici=generateEmployeeData(dtoIn);
-
-    let cetnost={};
+    let cetnost = {};
+    
+    // Spočítání četnosti jmen
     pracovnici.forEach(element => {
         if (cetnost[element.name] === undefined) {
             cetnost[element.name] = 1;
         } else {
             cetnost[element.name]++;
-        }   
-
+        }
     });
+
     console.log(cetnost);
-    let maxEntry = Object.entries(cetnost).reduce((max, entry) => {
-    return entry[1] > max[1] ? entry : max;
-      }, ["", -Infinity]);
-      
-      return maxEntry; 
-     
-      
+
+    // Získání maximální četnosti
+    const maxCount = Math.max(...Object.values(cetnost));
+
+    // Vyhledání všech jmen s maximální četností a jejich četnosti
+    let maxNames = Object.entries(cetnost)
+        .filter(([name, count]) => count === maxCount)
+        .map(([name, count]) => ({ name, count }));
+
+    return maxNames; 
 }
 
 const dtoIn = {
@@ -27,7 +31,8 @@ const dtoIn = {
         min: 20,
         max: 40
     },
-    count: 50// Počet zaměstnanců, které chceme vygenerovat
+    count: 50 // Počet zaměstnanců, které chceme vygenerovat
 };
-dtoOut=main(dtoIn)
-console.log(dtoOut)
+
+let dtoOut = main(dtoIn);
+console.log(dtoOut);
