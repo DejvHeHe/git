c3f5a3d9d633f01bb6d2fc248c3fbd5d136da728
David Sobíšek <dsobisek04@gmail.com>
2024-12-24 18:19:14 +0100

    Uprava piškvorek

---
+++ TicTacToe/script.js
@@ -0,0 +1,13 @@
+const winningCombinations = [
+    ['a1', 'a2', 'a3'],
+    ['b1', 'b2', 'b3'],
+    ['c1', 'c2', 'c3'],
+    ['a1', 'b1', 'c1'],
+    ['a2', 'b2', 'c2'],
+    ['a3', 'b3', 'c3'],
+    ['a1', 'b2', 'c3'],
+    ['a3', 'b2', 'c1']
+];
+const player1={};
+const player2={};
+
