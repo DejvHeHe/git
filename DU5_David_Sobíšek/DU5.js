f341cf51988eefe1eab36c7a8081cb29e7280e0f
David Sobíšek <dsobisek04@gmail.com>
2025-02-03 23:18:33 +0100

    Uprava  du5

--- DU5_David_Sobíšek/DU5.js
+++ DU5_David_Sobíšek/DU5.js
@@ -2,11 +2,9 @@ function main(dtoIn) {
     const generateEmployeeData = require('./DU3.js');
     // Kontrola validnosti vstupních dat
     if (dtoIn.count <= 0) {
-        // Pokud počet zaměstnanců není kladný
         return "Chyba-počet zaměstnancu neni spravný";
     }
     if (dtoIn.age.min >= dtoIn.age.max) {
-        // Pokud minimální věk je větší nebo roven maximálnímu
         return "Chyba-minimální věk je větší nebo roven maximálnímu";
     }
 
@@ -14,95 +12,84 @@ function main(dtoIn) {
     const listOfEmployees = generateEmployeeData(dtoIn);
 
     // Výpočet statistik zaměstnanců
-    const names = getEmployeeChartContent(listOfEmployees);
-    
-    const dtoOut = {
-        names: names // Vypočítané statistiky
-    };
+    const dtoOut = getEmployeeChartContent(listOfEmployees);
     return dtoOut;
-
-    
 }
-function getEmployeeChartContent(listOfEmployees)
-{
-    let allFrequency = {}; // Četnost všech jmen
-    let maleFrequency = {}; // Četnost pouze mužských jmen
-    let femaleFrequency={};
-    let maleFullTimeFrequency={};
-    let femalePartTimeFrequency={};
 
-    // Spočítání četnosti jmen
+function getEmployeeChartContent(listOfEmployees) {
+    let allFrequency = {};
+    let maleFrequency = {};
+    let femaleFrequency = {};
+    let maleFullTimeFrequency = {};
+    let femalePartTimeFrequency = {};
+
     listOfEmployees.forEach(element => {
-        // Četnost všech jmen
         if (allFrequency[element.name] === undefined) {
             allFrequency[element.name] = 1;
         } else {
             allFrequency[element.name]++;
         }
 
-        // Četnost mužských jmen
-        if (element.gender === 'male') { // Ověření pohlaví
+        if (element.gender === 'male') {
             if (maleFrequency[element.name] === undefined) {
                 maleFrequency[element.name] = 1;
             } else {
                 maleFrequency[element.name]++;
             }
-            if(element.workload===40)
-            {
+            if (element.workload === 40) {
                 if (maleFullTimeFrequency[element.name] === undefined) {
                     maleFullTimeFrequency[element.name] = 1;
                 } else {
                     maleFullTimeFrequency[element.name]++;
                 }
-
             }
-        }
-        else
-        { 
+        } else {
             if (femaleFrequency[element.name] === undefined) {
                 femaleFrequency[element.name] = 1;
             } else {
                 femaleFrequency[element.name]++;
             }
-            if(element.workload!==40)
-            {
+            if (element.workload !== 40) {
                 if (femalePartTimeFrequency[element.name] === undefined) {
                     femalePartTimeFrequency[element.name] = 1;
                 } else {
                     femalePartTimeFrequency[element.name]++;
                 }
-
             }
         }
     });
-    
-    
-   
-    console.log(JSON.stringify(allFrequency, null, 2));
-    // Návrat hodnot
+
     return {
-        all:sort(allFrequency),
-        male:sort(maleFrequency),
-        female:sort(femaleFrequency),
-        maleFullTime:sort(maleFullTimeFrequency),
-        femalePartTime:sort(femalePartTimeFrequency)
-
-
+        names: {
+            all: allFrequency,
+            male: maleFrequency,
+            female: femaleFrequency,
+            maleFullTime: maleFullTimeFrequency,
+            femalePartTime: femalePartTimeFrequency
+        },
+        chartData: {
+            all: convertToChartData(allFrequency),
+            male: convertToChartData(maleFrequency),
+            female: convertToChartData(femaleFrequency),
+            maleFullTime: convertToChartData(maleFullTimeFrequency),
+            femalePartTime: convertToChartData(femalePartTimeFrequency)
+        }
     };
-
-}
-function sort(range) {   
-    const objectSorted = Object.entries(range).sort((a, b) => b[1] - a[1]);
-    return Object.fromEntries(objectSorted); // Vrátí objekt místo pole
 }
 
+// Funkce pro převod frekvencí na formát { label, value }
+function convertToChartData(frequency) {
+    return Object.entries(frequency)
+        .sort((a, b) => b[1] - a[1]) // Seřazení podle hodnoty sestupně
+        .map(([label, value]) => ({ label, value })); // Převod na pole objektů
+}
 
 const dtoIn = {
     age: {
         min: 20,
         max: 40
     },
-    count: 15// Počet zaměstnanců, které chceme vygenerovat
+    count: 50 // Počet zaměstnanců, které chceme vygenerovat
 };
 
 let dtoOut = main(dtoIn);
