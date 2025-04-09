16d043d262e007250326616f9038e7cfdb7d9a58
David Sobíšek <dsobisek04@gmail.com>
2025-04-10 00:05:38 +0200

    fix

--- myApp/backend/dao/item-DAO.js
+++ myApp/backend/dao/item-DAO.js
@@ -3,6 +3,7 @@ require('dotenv').config(); // načte proměnné z .env
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
